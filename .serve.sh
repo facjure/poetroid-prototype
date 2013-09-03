ef0eb65b77a7d8090aeb8b57cb1f5cf1363e32dc
priyatam <priyatam@gmail.com>
2013-09-03 12:08:14 -0400

    updated folders

--- .serve.sh
+++ .serve.sh
@@ -1,5 +1,5 @@
 version = $1
-cp .build/index.html /Users/priyatam/Dev/github-pages/priyatam.com
-cd /Users/priyatam/Dev/github-pages/priyatam.com
+cp .build/index.html /Users/priyatam/Dev/github-pages/poetics
+cd /Users/priyatam/Dev/github-pages/poetics
 git commit index.html -m $1
 git push
