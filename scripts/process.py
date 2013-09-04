fddf3758bb6059b9e6bcaf0a9edbcd048231ea43
Sreeharsha Mudivarti <msreeharsha@gmail.com>
2013-09-04 19:25:09 +0530

    new poem/yaml error processor script #4

--- scripts/process.py
+++ scripts/process.py
@@ -1,81 +1,58 @@
 #!/usr/bin/env python
 """
 Usage:
-    ./process_poems.py <dropbox folder>
-
-YAML Problems:
-
--> -<space>
--> \t\n
--> \s*---(.+?)---\s*\n
-
-Poem Problems:
--> title
--> \t\n
+    ./process_poems.py <poems folder>
 """
-import yaml
+import os, sys
+
+folder = sys.argv[1]
+errors_folder = folder + os.sep + "errors"
+
+print """Processing -> """ + folder
+
+os.system("rm except.log.md")
+log = open("except.log.md", "a+")
+
 import re
+import yaml
 import StringIO
 from glob import glob
-import os
-import sys
 from pipes import quote
 from codecs import open
+from datetime import datetime
+
+import cleaners
 
 
-def yaml_and_poem(poem_text):
-    match = re.search(u"\s*---(.+?)---[ ]*\n", poem_text, flags=re.U | re.S)
-    yaml = match.group(1)
-    match_len = len(match.group(0))
-    poem_text = poem_text[match_len:]
-    return yaml, poem_text
+def split_file(poem_text):
+    match = re.split(u"\n---[ ]*\n", poem_text, flags=re.U | re.S)
+    yaml_text = match[0]
+    poem = match[1]
+    return yaml_text, poem
 
 
-def clean_yaml(yaml_text):
-    yaml_text = re.sub(r'[ \t]+\n', "\n", yaml_text)
-    yaml_text = re.sub(r'\*', "", yaml_text)
-    yaml_text = re.sub(r'-(.)', r'- \1', yaml_text)
-    return yaml_text
-
-
-def clean_poem(text):
-    text = re.sub(r'[ \t]+\n', "\n", text)
-    text = re.sub(r'\r\n?|\n', "\n", text)  # \r is Mac OSX line ending
-    text = re.sub(r'\n{2,}', "\n\n", text)
-    text = re.sub(r'^\s*?(.+?)\n\n', "", text)  # remove title if any
-    return text
-
-
-def clean_name(txtfile):
-    new_txtfile_name = txtfile[len('raw/'):]  # Get rid of "raw/"filename
-    new_txtfile_name = re.sub(r'[\'",]', "-", new_txtfile_name)
-    new_txtfile_name = re.sub(" ", "-", new_txtfile_name)
-    new_txtfile_name = re.sub("-+", "-", new_txtfile_name)
-    return new_txtfile_name
-
-os.system("rm -rf raw")
-os.system("cp -R " + quote(sys.argv[1]) + " raw")
-
-os.system("rm except.log")
-log = open("except.log", "a")
-
-for txtfile in glob("raw/*"):
-    print txtfile
+for txtfile in glob(folder + os.sep + "*.txt"):
+    print "file -> " + txtfile
     try:
+        txtfile_name = os.path.basename(txtfile)
+        if not cleaners.is_clean_name(txtfile_name):
+            raise Exception("Filenames should have hyphens only. \"<authorLastName>-<first-five-words-of-title>.txt\". Use - for all special characters.")
         text = open(txtfile, "r", "utf-8").read()
-        yaml_text, poem = yaml_and_poem(text)
-        yaml_cleaned = clean_yaml(yaml_text)
-        yaml.load(StringIO.StringIO(yaml_cleaned))
-        cleaned_poem = clean_poem(poem)
-        final_filename = clean_name(txtfile)
-        ffh = open("processed/" + final_filename, "w", "utf-8")
-        ffh.write(u"---" + yaml_cleaned + u"---\n" + cleaned_poem)
-        ffh.close()
+        yaml_text, poem = split_file(text)
+        if len(poem) < 10: 
+            raise Exception("Fault in process.py or Poem is too small")
+        yaml.load(StringIO.StringIO(yaml_text))
     except Exception, error:
-        log.write("Error in \"" + txtfile + "\": " + str(error) + "\n")
-        cmd = "cp " + quote(txtfile) + " errors/"
-        print cmd
+        log.write("#### Error in \"" + txtfile + "\"\n" + str(error) + "\n\n")
+        cmd = "mv " + quote(txtfile) + " " +  quote(errors_folder)
+        print "    " + cmd
         os.system(cmd)
         continue
 
 log.close()
+log = open("except.log.md", "r")
+if len(log.readlines()) > 2:
+    log.close()
+    cmd = "mv except.log.md " + quote(errors_folder + os.sep + "except.log.UTC." + str(datetime.utcnow()) + ".md")
+    print "    " + cmd
+    os.system(cmd)
