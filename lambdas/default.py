46e0b75495dfdf5109e0d8309a19ab1fcb5d253d
Sreeharsha Mudivarti <msreeharsha@gmail.com>
2013-09-05 23:22:03 +0530

    added by authors template

--- lambdas/default.py
+++ lambdas/default.py
@@ -1,4 +1,5 @@
 import pystache
+import urllib
 
 def hello_world(text):
     return "Hello %s from Frozen Pie" % text
@@ -6,3 +7,19 @@ def hello_world(text):
 def all_posts(text):
     renderer = pystache.Renderer(file_encoding="utf-8", string_encoding="utf-8")
     return renderer.render(text, {"posts": contents })
+
+def by_author(text):
+    renderer = pystache.Renderer(file_encoding="utf-8", string_encoding="utf-8")
+    authors = []
+    for poem in contents:
+        author = poem.get("author", "Anonymous")
+        if author is None:
+            author = "Anonymous"
+
+        matches = filter(lambda x: x["name"] == author, authors)
+        
+        if not matches:
+            authors.append({"name": author, "link_name": '#/search?query=author' + urllib.quote_plus(":" +  author)})
+
+    authors.sort(key= lambda d : d.get("name"))
+    return renderer.render(text,{"authors": authors})
