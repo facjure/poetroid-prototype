655ac8cb15a86e0aab0e5b75d779c992795a1a10
Sreeharsha Mudivarti <msreeharsha@gmail.com>
2013-09-05 17:13:53 +0530

    script for cronjob is ready

--- scripts/process.py
+++ scripts/process.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 """
 Usage:
-    ./process_poems.py <poems folder>
+    process scripts/process.py
+
+Assumptions
+1) Always run from poetics directory
+2) virtual env for frozen-pie in in the same folder as frozen pie
+3) paths are hardcoded
 """
-import os, sys
 
-folder = sys.argv[1]
-errors_folder = folder + os.sep + "errors"
+poetics_path = "/Users/harsha/yuti/poetics"
+frozen_pie_path = "/Users/harsha/yuti/frozen-pie-latest"
+poems_path = "/Users/harsha/yuti/poems"
+errors_path = poems_path + "/errors"
+github_url = "git@github.com:Facjure/poetics.git"
 
-print """Processing -> """ + folder
+# get latest poems if any
+import os
+cmd = "cd " + poems_path + "; git pull origin master"
+os.system(cmd)
 
 os.system("rm except.log.md")
-log = open("except.log.md", "a+")
+log = open("except.log.md", "a")
 
+import sys
 import re
 import yaml
 import StringIO
@@ -20,9 +31,9 @@ from glob import glob
 from pipes import quote
 from codecs import open
 from datetime import datetime
+from dateutil import tz
 
 import cleaners
-
 
 def split_file(poem_text):
     match = re.split(u"\n---[ ]*\n", poem_text, flags=re.U | re.S)
@@ -30,9 +41,9 @@ def split_file(poem_text):
     poem = match[1]
     return yaml_text, poem
 
+print """Processing poems"""
 
-for txtfile in glob(folder + os.sep + "*.txt"):
-    print "file -> " + txtfile
+for txtfile in glob(poems_path + os.sep + "*.txt"):
     try:
         txtfile_name = os.path.basename(txtfile)
         if not cleaners.is_clean_name(txtfile_name):
@@ -44,15 +55,42 @@ for txtfile in glob(folder + os.sep + "*.txt"):
         yaml.load(StringIO.StringIO(yaml_text))
     except Exception, error:
         log.write("#### Error in \"" + txtfile + "\"\n" + str(error) + "\n\n")
-        cmd = "mv " + quote(txtfile) + " " +  quote(errors_folder)
+        cmd = "mv " + quote(txtfile) + " " +  quote(errors_path)
         print "    " + cmd
         os.system(cmd)
         continue
+
+print "Done"
+
 
 log.close()
 log = open("except.log.md", "r")
 if len(log.readlines()) > 2:
     log.close()
-    cmd = "mv except.log.md " + quote(errors_folder + os.sep + "except.log.UTC." + str(datetime.utcnow()) + ".md")
+
+    utc = datetime.utcnow()
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz('America/New_York')
+    utc = utc.replace(tzinfo=from_zone)
+    boston_time = utc.astimezone(to_zone)
+    print str(boston_time)
+
+    cmd = "mv except.log.md " + quote(errors_path + os.sep + "except.log.BostonTime." + str(boston_time) + ".md")
     print "    " + cmd
     os.system(cmd)
+
+    os.chdir(poems_path)
+    os.system("git add -A; git commit -m 'BuildBot " + str(boston_time) + "'")
+    os.system("git push origin master")
+    os.chdir(frozen_pie_path)
+    os.system("./env/bin/python bake.py --config " + poetics_path + os.sep + "config.yml")
+    os.chdir(poetics_path)
+    os.system("mkdir deploy")
+    os.system("mv .build/index.html deploy/")
+    os.system("rm -rf .build")
+    os.system("git clone -b gh-pages " + github_url + " .build")
+    os.system("cp deploy/index.html .build/")
+    os.system("cd .build; git add index.html; git commit -m 'new deploy " + str(boston_time) + "'; git push --force origin gh-pages")
+
+
+    
