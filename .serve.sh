2b8d0f4093de2f662e9ceb336362df6393ec2ae3
priyatam <priyatam@gmail.com>
2013-10-28 15:28:01 -0700

    refactor scripts

--- .serve.sh
+++
@@ -1,4 +0,0 @@
-cp .build/index.html /Users/priyatam/Dev/github-pages/poetics
-cd /Users/priyatam/Dev/github-pages/poetics
-git commit index.html -m $1
-git push
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 cd ../frozen-pie
-bake.py --config /Users/priyatam/Dev/github/facjure/poetics/config.yml
-cd ../poetics
+bake.py --config /Users/priyatam/Dev/github/facjure/poetroid/client/config.yml
+cd ../poetroid/client
 cp js/* .build/*
 cd .build
 python -m SimpleHTTPServer
-
+open -a "/Applications/Google Chrome.app" .build/index.html
