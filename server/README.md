bac1db549aa58db0d11fadc4a737b6c3dfea7590
priyatam <priyatam@gmail.com>
2013-11-01 15:21:30 -0700

    added base compojure app

--- server/README.md
+++ server/README.md
@@ -1,1 +1,13 @@
-TODO
+# example
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+`lein run`
+
+## License
+
+Copyright © 2013 Adam Bard
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ server/src/poetroid/core.clj
@@ -0,0 +1,28 @@
+(ns poetroid.core
+  (:use compojure.core)
+  (:require
+    [compojure.route :as route]
+    [ring.adapter.jetty :as jetty]
+    [clostache.parser :as clostache]))
+
+; Template Rendering
+(defn read-template [template-name]
+  (slurp (clojure.java.io/resource (str "templates/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+   (clostache/render (read-template template-file) params))
+
+; View functions
+(defn index []
+  (render-template "index" {:greeting "Discover Poetry Together"}))
+
+; Routing
+(defroutes main-routes
+  (GET "/" [] (index))
+  (route/resources "/")
+  (route/not-found "404 Not Found"))
+
+; Server
+(defn -main []
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "5000"))]
+    (jetty/run-jetty main-routes {:port port})))
