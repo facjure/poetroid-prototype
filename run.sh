fb53d9d5fc41f3d14de6b74986bfe580104b6fe8
priyatam <priyatam@gmail.com>
2013-11-03 14:48:36 -0800

    refactor run

--- run.sh
+++
@@ -1,7 +0,0 @@
-cd ../frozen-pie
-bake.py --config /Users/priyatam/Dev/github/facjure/poetroid/client/config.yml
-cd ../poetroid/client
-cp js/* .build/*
-cd .build
-python -m SimpleHTTPServer
-open -a "/Applications/Google Chrome.app" .build/index.html
