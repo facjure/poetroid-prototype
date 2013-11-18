2a4f141bbcf7d7a9898c216d9a856867276037de
Sreeharsha Mudivarti <msreeharsha@gmail.com>
2013-11-18 23:02:25 +0530

    added resume feature

--- scripts/scraper/pdp.py
+++ scripts/scraper/pdp.py
@@ -28,7 +28,7 @@ ignored_links = ['<a href="/"><b>Home</b></a>',
 html_dir = "html/"
 
 def write_file(name, text, directory=None):
-    if dir and not os.path.exists(html_dir + directory):
+    if directory and not os.path.exists(html_dir + directory):
         os.makedirs(html_dir + directory)
     filehandle = open(name, 'w', "utf-8")
     filehandle.write(text)
@@ -48,27 +48,32 @@ def save_link(link, char, pageno):
     ro = re.search(r'href="(.+?)"', link)
     link = "http://public-domain-poetry.com/" + ro.group(1)
     r = requests.get(link, headers=headers)
+    if r.status_code == 404:
+        print "FAILED AT " + char + " --- " + str(pageno)
+        exit(1)
     r.encoding = 'utf-8'
-    filename = html_dir + char + "/" + str(pageno) + "/" + link.rsplit('/',1) + " - " + datetime.now()
+    filename = html_dir + char + "/" + str(pageno) + "/" + link.rsplit('/',1)[-1] + " - " + str(datetime.now())
     write_file(filename, r.text)
-    time.sleep(20)
+    time.sleep(5)
 
 def get_page(char, pageno):
-    link = generateazlink(char,1)
+    link = generateazlink(char,pageno)
     r = requests.get(link, headers=headers)
+    if r.status_code == 404:
+        print "FAILED AT " + char + " --- " + str(pageno)
+        exit(1)
     r.encoding = 'utf-8'
     filename = html_dir + char + "/1.html"
     dirforpoems = char + "/1"
     write_file(filename, r.text, dirforpoems)
-    return r.text
-
-def get_first_page(char):
-    text = get_page(char, 1)
-    bso = bs(text, "html5lib")
+    bso = bs(r.text, "html5lib")
     return bso
 
-def get_rest_pages(first_page_bso, char):
-    current_page = first_page_bso
+def get_first_page(char, pageno):
+    return bso
+
+def get_pages(char, pageno):
+    current_page = get_page(char, pageno)
     required_links = current_page.find_all('table')[10].find_all('a')
     lastpage_link = required_links[-1]
     ro = re.search(r'(\d+)"><', str(lastpage_link))
@@ -91,9 +96,12 @@ def get_rest_pages(first_page_bso, char):
         required_links = current_page.find_all('table')[10].find_all('a')
         lastpage_link = required_links[-1]
 
+skip_alpha = sys.argv[1]
+pageno = sys.argv[2]
 
 for char in string.ascii_uppercase:
+    if ord(char) < ord(skip_alpha):
+        continue
     import pdb; pdb.set_trace()
-    first_page_bso = get_first_page(char)
-    get_rest_pages(first_page_bso, char)
+    get_pages(char, int(pageno))
 
