ea6104199a440668fb069b168ef5bd685fc40d02
priyatam <priyatam@gmail.com>
2013-12-22 23:22:46 -0800

    cleanup

--- .run.sh
+++ .run.sh
@@ -1,7 +1,3 @@
-cd ../literatte/pie
-pie.py --config /Users/priyatam/Dev/github/facjure/poetroid/client/config.yml
-cd ../poetroid/client
-cp js/* .build/*
-cd .build
-python -m SimpleHTTPServer
-open -a "/Applications/Google Chrome.app" .build/index.html
+lit.py /Users/priyatam/Dev/github/facjure/poetroid/client/ /Users/priyatam/Dev/github/facjure/poetroid/client/poems
+
+open -a "/Applications/Google Chrome.app" site/index.html
