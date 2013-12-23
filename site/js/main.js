47ea1f0d19e0a6fad4f999a2a991a484a493bf6f
priyatam <priyatam@gmail.com>
2013-12-23 00:04:09 -0800

    including main explicitly

--- site/js/main.js
+++ site/js/main.js
@@ -269,7 +269,17 @@ $( function () {
 
     });
 
-    app.run()
+     // flowtype typography
+    $('body').flowtype({
+        minimum   : 500,
+        maximum   : 1600,
+        minFont   : 12,
+        maxFont   : 40,
+        fontRatio : 30,
+        lineRatio : 1.45
+    });
+
+    app.run();
 });
 
 
